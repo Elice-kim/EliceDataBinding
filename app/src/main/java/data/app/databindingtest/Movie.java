ad9b06a4cfeee7019165583a5a6b62529d5c2b93
Elice-kim <grit_julia@naver.com>
2017-12-03 21:01:58 +0900

    remove unnecessary BaseObservable because of ObservableArrayList

--- app/src/main/java/data/app/databindingtest/Movie.java
+++ app/src/main/java/data/app/databindingtest/Movie.java
@@ -1,13 +1,10 @@
 package data.app.databindingtest;
 
-import android.databinding.BaseObservable;
-import android.databinding.Bindable;
-
 /**
  * Created by elice.kim on 2017. 12. 3..
  */
 
-public class Movie extends BaseObservable{
+public class Movie {
 
     private String title, genre, year;
     public Movie(String title, String genre, String year) {
@@ -16,16 +13,13 @@ public class Movie extends BaseObservable{
         this.year = year;
     }
 
-    @Bindable
     public String getTitle() {
         return title;
     }
 
-    @Bindable
     public String getYear() {
         return year;
     }
-    @Bindable
     public String getGenre() {
         return genre;
     }
